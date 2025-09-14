ebf0ae8529680f7a3d9436ea9d0fb37325f1e2cb
Oluwaseyi <seyistry@gmail.com>
2025-09-14 20:10:30 +0100

    hide header

--- prodev-mobile-app-0x06/app/(home)/layout.tsx
+++ prodev-mobile-app-0x06/app/(home)/layout.tsx
@@ -28,7 +28,6 @@ const HomeRootLayout = () => {
       }} />
         <Tabs.Screen name="profile" options={{
         title: 'Profile',
-        headerShown: false,
         tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
       }} />
     </Tabs>
